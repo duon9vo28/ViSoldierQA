8eff32f1688afe7219050d6d055de3006bfd3af0
duong28022003 <duong922003@gmail.com>
2025-05-11 12:12:28 +0700

    Adjust your own API key

--- config/ModelConfig.py
+++ config/ModelConfig.py
@@ -5,10 +5,10 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent))
 
 class APIConfig:
-    LANGSMITH_API_KEY = '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'
-    GROQ_API_KEY = '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'
-    QDRANT_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.NuM4zPgiBXg-VHdCkBlVaWVHoS-Dn_6qWU9tQHnF6nM'
-    QDRANT_URL = 'https://19cddb00-1635-4336-b06c-6a4605f48e6f.europe-west3-0.gcp.cloud.qdrant.io:6333'
+    LANGSMITH_API_KEY = 'Your-Key'
+    GROQ_API_KEY = 'Your-Key'
+    QDRANT_API_KEY = 'Your-Key.eyJhY2Nlc3MiOiJtIn0.NuM4zPgiBXg-VHdCkBlVaWVHoS-Dn_6qWU9tQHnF6nM'
+    QDRANT_URL = 'Your-Url'
     QDRANT_COLLECTION_NAME = 'Vietnamese_Soliders_chunks'
     TOP_K = 20
     CHAT_HISTORY = 10
